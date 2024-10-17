3bb43e37730cab949d9824f98ce8546c7da8477c
Emanuel Campos Jasso <34528183+JassoEC@users.noreply.github.com>
2024-10-17 12:20:35 -0600

    remove api-gateway folder

--- api-gateway/proto-gen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Generate the gRPC code
-protoc  --proto_path=../proto --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./src/types  --ts_proto_opt=nestJs=true products.proto orders.proto
